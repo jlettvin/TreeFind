2e77e24ebfd05da300610e45c9749a0c531409b1
Jonathan D. Lettvin <jlettvin@gmail.com>
2016-11-07 15:12:58 -0500

    Added module __doc__ strings and legal constants

--- UniArray.py
+++ UniArray.py
@@ -9,6 +9,19 @@ Justification: UniDigit uses a multidimensional array.
 This class will be used to manage that array.
 """
 
+__module__     = "UniArray.py"
+__author__     = "Jonathan D. Lettvin"
+__copyright__  = "\
+Copyright(C) 2016 Jonathan D. Lettvin, All Rights Reserved"
+__credits__    = [ "Jonathan D. Lettvin" ]
+__license__    = "GPLv3"
+__version__    = "0.0.1"
+__maintainer__ = "Jonathan D. Lettvin"
+__email__      = "jlettvin@gmail.com"
+__contact__    = "jlettvin@gmail.com"
+__status__     = "Demonstration"
+__date__       = "20161107"
+
 from ujson  import dumps
 
 class UniArray(dict):
--- UniDict.py
+++ UniDict.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+"""UniDict.py
+This class converts dictionary element names into object member names
+It strips out all non-alpha characters before this conversion.
+
+Justification: Most of these modules make use of simplified dictionary access.
+"""
+
+__module__     = "UniDict.py"
+__author__     = "Jonathan D. Lettvin"
+__copyright__  = "\
+Copyright(C) 2016 Jonathan D. Lettvin, All Rights Reserved"
+__credits__    = [ "Jonathan D. Lettvin" ]
+__license__    = "GPLv3"
+__version__    = "0.0.1"
+__maintainer__ = "Jonathan D. Lettvin"
+__email__      = "jlettvin@gmail.com"
+__contact__    = "jlettvin@gmail.com"
+__status__     = "Demonstration"
+__date__       = "20161107"
+
 from pprint import pprint
 from ujson  import dumps
 
